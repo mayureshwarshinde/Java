af0bcac9a0bbbba59becabbc30bbf31a8df4e7ec
Mayureshwar-Shinde <mayureshwarshinde@gmail.com>
2025-07-12 23:11:06 +0530

    step-4 : 2

---
+++ STEP4/2/Two.java
@@ -0,0 +1,27 @@
+package hashing;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Two {
+	public static boolean check(int[] a, int[] b) {
+		Map<Integer, Boolean> map = new HashMap<>();
+		
+		for(int i = 0; i < a.length; i++) {
+			map.put(a[i], true);
+		}
+		
+		for(int i = 0; i < b.length; i++) {
+			if(!map.containsKey(b[i])) return false;
+		}
+		
+		return true;
+	}
+
+	public static void main(String[] args) {
+		int[] a = {11, 1, 13, 21, 3, 7};
+		int[] b = {11, 3, 7, 1};
+		
+		System.out.println("b subset of a? : " + check(a, b));
+	}
+}
